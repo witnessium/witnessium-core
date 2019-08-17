41aa6a9151244e92c1d4904d61ef3bd21c990c5b
sungkmi <sungkmi@gmail.com>
2019-08-18 03:46:18 +0900

    Implement MerkleTrie

---
+++ node/src/main/scala/org/witnessium/core/node/crypto/MerkleTrie.scala
@@ -0,0 +1,308 @@
+package org.witnessium.core
+package node
+package crypto
+
+import cats.Monad
+import cats.data.{EitherT, StateT}
+import cats.implicits._
+import eu.timepit.refined.refineV
+import eu.timepit.refined.api.Refined
+import scodec.bits.BitVector
+import shapeless.nat._16
+import shapeless.syntax.sized._
+import codec.byte.{ByteDecoder, ByteEncoder, DecodeResult}
+import datatype.{MerkleTrieNode, UInt256Bytes}
+import org.witnessium.core.util.refined.bitVector._
+
+object MerkleTrie {
+  type Hash = UInt256Bytes
+  
+  @SuppressWarnings(Array("org.wartremover.warts.Recursion"))
+  def get[F[_]:NodeStore:Monad, A: ByteDecoder](
+    key: BitVector
+  ): StateT[EitherT[F, String, *], MerkleTrieState, Option[A]] = StateT.inspectF{ (state: MerkleTrieState) =>
+    if (state.root.isEmpty) EitherT.rightT[F, String](None) else for {
+      node <- getNode(state)
+      valueOption <- (node match {
+        case MerkleTrieNode.Leaf(prefix, value) if key === prefix.value =>
+          EitherT.fromEither[F](ByteDecoder[A].decode(value).flatMap{
+            case DecodeResult(value, remainder) if remainder.isEmpty => Right(Some(value))
+            case result => Left(s"Non empty remainder after decoding: $result")
+          })
+        case MerkleTrieNode.Branch(prefix, children) if (key startsWith prefix.value) && key.size >= prefix.value.size + 4 =>
+          val (index, nextKey) = key.drop(prefix.value.size).splitAt(4)
+          children.unsized(index.toInt(signed = false)) match {
+            case Some(nextRoot) =>
+              get(nextKey) runA state.copy(root = Some(nextRoot))
+            case None =>
+              EitherT.rightT[F, String](None)
+          }
+        case _ => EitherT.rightT[F, String](None)
+      })
+    } yield valueOption
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.Recursion"))
+  def put[F[_]:NodeStore:Monad, A: ByteEncoder](
+    key: BitVector, value: A
+  ): StateT[EitherT[F, String, *], MerkleTrieState, Unit] = StateT.modifyF((state: MerkleTrieState) => state.root match {
+    case None =>
+      val leaf =  MerkleTrieNode.Leaf(ensurePrefix(key), ByteEncoder[A].encode(value))
+      val leafHash = hash[MerkleTrieNode](leaf)
+      EitherT.rightT[F, String](state.copy(
+        root = Some(leafHash),
+        diff = state.diff.add(leafHash, leaf),
+      ))
+    case Some(root) =>
+      getNode(state).flatMap{ (node: MerkleTrieNode) =>
+
+        val prefix0: BitVector = node.prefix.value
+        val commonPrefixNibbleSize: Int = (key ^ prefix0).grouped(4L).takeWhile(_ === BitVector.low(4L)).size
+        val nextPrefixBitSize = ((key.size / 4L - 1L) min commonPrefixNibbleSize.toLong) * 4L
+        val (commonPrefix, remainder1) = key splitAt nextPrefixBitSize
+        val (index1, prefix1) = remainder1 splitAt 4L
+        val remainder0 = prefix0 drop nextPrefixBitSize
+
+        node match {
+          case MerkleTrieNode.Leaf(_, value0) =>
+            val (index00, prefix00) = remainder0 splitAt 4L
+            val leaf0 = MerkleTrieNode.Leaf(ensurePrefix(prefix00), value0)
+            val leaf0hash = hash[MerkleTrieNode](leaf0)
+            val leaf1 = MerkleTrieNode.Leaf(ensurePrefix(prefix1), ByteEncoder[A].encode(value))
+            val leaf1hash = hash[MerkleTrieNode](leaf1)
+            val branch = MerkleTrieNode.Branch(ensurePrefix(commonPrefix),
+              Vector.fill(16)(None)
+                .updated(index00.toInt(signed = false), Some(leaf0hash))
+                .updated(index1.toInt(signed = false), Some(leaf1hash))
+                .ensureSized[_16]
+            )
+            val branchHash = hash[MerkleTrieNode](branch)
+            EitherT.rightT[F, String](MerkleTrieState(
+              root = Some(branchHash),
+              diff = state.diff.add(branchHash, branch).add(leaf0hash, leaf0).add(leaf1hash, leaf1).remove(root),
+            ))
+          case MerkleTrieNode.Branch(_, children) if remainder0.isEmpty =>
+            children.unsized(index1.toInt(signed = false)) match {
+              case None =>
+                val leaf1 = MerkleTrieNode.Leaf(ensurePrefix(prefix1), ByteEncoder[A].encode(value))
+                val leaf1hash = hash[MerkleTrieNode](leaf1)
+                val branch = MerkleTrieNode.Branch(ensurePrefix(commonPrefix),
+                  children.unsized
+                    .updated(index1.toInt(signed = false), Some(leaf1hash))
+                    .ensureSized[_16]
+                )
+                val branchHash = hash[MerkleTrieNode](branch)
+                EitherT.rightT[F, String](MerkleTrieState(
+                  root = Some(branchHash),
+                  diff = state.diff.add(branchHash, branch).add(leaf1hash, leaf1).remove(root),
+                ))
+              case Some(childHash) =>
+                put(prefix1, value) runS state.copy(root = Some(childHash)) map { childState =>
+                  val branch = MerkleTrieNode.Branch(node.prefix,
+                    children.unsized
+                      .updated(index1.toInt(signed = false), childState.root)
+                      .ensureSized[_16]
+                  )
+                  val branchHash = hash[MerkleTrieNode](branch)
+                  childState.copy(
+                    root = Some(branchHash),
+                    diff = childState.diff.add(branchHash, branch).remove(root)
+                  )
+                }
+            }
+          case MerkleTrieNode.Branch(_, children) =>
+            val (index00, prefix00) = remainder0 splitAt 4L
+            val branch00 = MerkleTrieNode.Branch(ensurePrefix(prefix00), children)
+            val branch00hash = hash[MerkleTrieNode](branch00)
+            val leaf1 = MerkleTrieNode.Leaf(ensurePrefix(prefix1), ByteEncoder[A].encode(value))
+            val leaf1hash = hash[MerkleTrieNode](leaf1)
+            val branch = MerkleTrieNode.Branch(ensurePrefix(commonPrefix),
+              Vector.fill(16)(None)
+                .updated(index00.toInt(signed = false), Some(branch00hash))
+                .updated(index1.toInt(signed = false), Some(leaf1hash))
+                .ensureSized[_16]
+            )
+            val branchHash = hash[MerkleTrieNode](branch)
+            EitherT.rightT[F, String](MerkleTrieState(
+              root = Some(branchHash),
+              diff = state.diff.add(branchHash, branch).add(branch00hash, branch00).add(leaf1hash, leaf1).remove(root),
+            ))
+        }
+      }
+  })
+
+  def remove[F[_]: NodeStore: Monad, A: ByteEncoder: ByteDecoder](value: A): StateT[EitherT[F, String, *], MerkleTrieState, Unit] =
+    removeByKey(crypto.hash(value).bits)
+
+  @SuppressWarnings(Array("org.wartremover.warts.Recursion","org.wartremover.warts.NonUnitStatements"))
+  def removeByKey[F[_]: NodeStore: Monad, A: ByteDecoder](
+    key: BitVector
+  ): StateT[EitherT[F, String, *], MerkleTrieState, Unit] = StateT.modifyF((state: MerkleTrieState) => state.root match {
+    case None => EitherT.leftT[F, MerkleTrieState](s"Fail to remove element from empty merkle trie: $state")
+    case Some(root) => getNode(state).flatMap{
+      case MerkleTrieNode.Leaf(prefix, _) if prefix.value === key =>
+        EitherT.rightT[F, String](state.copy(
+          root = None,
+          diff = state.diff.remove(root),
+        ))
+      case MerkleTrieNode.Leaf(_, _) =>
+        EitherT.leftT[F, MerkleTrieState](s"Fail to remove element: $key does not exist")
+      case MerkleTrieNode.Branch(prefix, children) =>
+        val commonPrefixNibbleSize: Int = (key ^ prefix.value).grouped(4L).takeWhile(_ === BitVector.low(4L)).size
+        val nextPrefixBitSize = commonPrefixNibbleSize.toLong * 4L
+        val remainder1 = key drop nextPrefixBitSize
+
+        if (remainder1.size < 4L) EitherT.leftT[F, MerkleTrieState](s"Fail to remove element: $key does not exist")
+        else {
+          val (index1, key1) = remainder1 splitAt 4L
+          children.unsized(index1.toInt(signed = false)) match {
+            case None => EitherT.leftT[F, MerkleTrieState](s"Fail to remove element: $key does not exist")
+            case Some(childHash) => removeByKey(key1) runS state.copy(root = Some(childHash)) flatMap {
+              case childState if childState.root.isEmpty && children.unsized.count(_.nonEmpty) <= 1 =>
+                EitherT.rightT[F, String](childState.copy(
+                  root = None,
+                  diff = childState.diff.remove(root),
+                ))
+              case childState =>
+                val branch = MerkleTrieNode.Branch(prefix,
+                  children.unsized
+                    .updated(index1.toInt(signed = false), childState.root)
+                    .ensureSized[_16]
+                )
+                val branchHash = hash[MerkleTrieNode](branch)
+
+                compact runS MerkleTrieState(
+                  root = Some(branchHash),
+                  diff = childState.diff.add(branchHash, branch).remove(root),
+                )
+              }
+          }
+        }
+    }
+  })
+
+  @SuppressWarnings(Array("org.wartremover.warts.Recursion"))
+  def from[F[_]:NodeStore:Monad, A: ByteDecoder](
+    key: BitVector
+  ): StateT[EitherT[F, String, *], MerkleTrieState, List[(BitVector, A)]] = {
+    StateT.inspectF((state: MerkleTrieState) => state.root match {
+      case None => EitherT.rightT[F, String](List.empty)
+      case Some(_) => getNode(state).flatMap{
+        case MerkleTrieNode.Leaf(prefix, value) =>
+          if (key <= prefix.value) EitherT.fromEither[F](ByteDecoder[A].decode(value).flatMap{
+            case DecodeResult(v, remainder) if remainder.isEmpty => Right(List((prefix.value, v)))
+            case result => Left(s"Decoding failure: nonEmpty remainder $result")
+          })
+          else EitherT.rightT[F, String](List.empty)
+        case MerkleTrieNode.Branch(prefix, children) =>
+
+          def runFrom(key: BitVector)(hashWithIndex: (Option[Hash], Int)): EitherT[F, String, List[(BitVector, A)]] = {
+            from(key) runA state.copy(root = hashWithIndex._1) map (_.map{ case (key, a) =>
+              (prefix.value ++ BitVector.fromInt(hashWithIndex._2, 4) ++ key, a)
+            })
+          }
+
+          if (key <= prefix.value) children.unsized.toList.zipWithIndex traverse runFrom(BitVector.empty) map(_.flatten)
+          else if (!prefix.value.startsWith(key)) EitherT.rightT[F, String](List.empty)
+          else {
+            val (index1, key1) = key drop prefix.value.size splitAt 4L
+            val targetChildren: List[(Option[Hash], Int)] = children.unsized.toList.zipWithIndex.drop(index1.toInt(signed = false))
+            targetChildren match {
+              case Nil => EitherT.rightT[F, String](List.empty)
+              case x :: xs =>
+                for {
+                  headList <- runFrom(key1)(x)
+                  tailList <- xs traverse runFrom(BitVector.empty)
+              } yield headList ::: tailList.flatten
+            }
+          }
+      }
+    })
+  }
+
+  def getNode[F[_]:NodeStore:Monad](state: MerkleTrieState): EitherT[F, String, MerkleTrieNode] = for {
+    root <- EitherT.fromOption[F](state.root, s"Cannot get node from empty merkle trie: $state")
+    nodeOption <- EitherT.fromEither[F](Either.cond(!(state.diff.removal contains root),
+      state.diff.addition.get(root),
+      s"Merkle trie node is removed: $state"
+    ))
+    node <- nodeOption.fold[EitherT[F, String, MerkleTrieNode]]{
+      EitherT(NodeStore[F].get(root)).subflatMap[String, MerkleTrieNode]{
+        _.toRight(s"Merkle trie node is not found: $state")
+      }
+    }(EitherT.rightT[F, String](_))
+  } yield node
+
+  type PrefixBits = BitVector Refined MerkleTrieNode.PrefixCondition
+
+  @SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
+  def ensurePrefix(bits: BitVector): PrefixBits = {
+    refineV[MerkleTrieNode.PrefixCondition](bits).toOption.get
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.Recursion"))
+  def compact[F[_]: NodeStore: Monad, A]: StateT[EitherT[F, String, *], MerkleTrieState, Unit] = {
+    StateT.modifyF((state: MerkleTrieState) => state.root match {
+      case None => EitherT.rightT[F, String](state)
+      case Some(root) => getNode(state).flatMap {
+        case MerkleTrieNode.Branch(prefix, children) =>
+          children.unsized.zipWithIndex.filter(_._1.nonEmpty) match {
+            case Vector() =>
+              EitherT.rightT[F, String](state.copy(
+                root = None,
+                diff = state.diff.remove(root),
+              ))
+            case Vector((Some(childHash), index)) =>
+              def getPrefix(local: PrefixBits): PrefixBits = ensurePrefix(prefix.value ++ BitVector.fromInt(index, 4) ++ local.value)
+              compact runS state.copy(root = Some(childHash)) flatMap { childState =>
+                getNode(childState).map {
+                  case MerkleTrieNode.Leaf(prefix0, value) =>
+                    val nextLeaf = MerkleTrieNode.Leaf(getPrefix(prefix0), value)
+                    val nextLeafHash = hash[MerkleTrieNode](nextLeaf)
+                    MerkleTrieState(
+                      root = Some(nextLeafHash),
+                      diff = childState.diff.add(nextLeafHash, nextLeaf).remove(root),
+                    )
+                  case MerkleTrieNode.Branch(prefix0, children) =>
+                    val nextBranch = MerkleTrieNode.Branch(getPrefix(prefix0), children)
+                    val nextBranchHash = hash[MerkleTrieNode](nextBranch)
+                    MerkleTrieState(
+                      root = Some(nextBranchHash),
+                      diff = childState.diff.add(nextBranchHash, nextBranch).remove(root),
+                    )
+                }
+              }
+            case _ =>
+              EitherT.rightT[F, String](state)
+          }
+        case _ => EitherT.rightT[F, String](state)
+      }
+    })
+  }
+
+  implicit class BitVectorCompare(val bits: BitVector) extends AnyVal {
+    def <=(that: BitVector): Boolean = {
+      val xor = bits ^ that
+      if (xor.populationCount === 0L) bits.size <= that.size
+      else ~(bits implies that).populationCount === 0L
+    }
+  }
+
+  trait NodeStore[F[_]] {
+    def get(hash: Hash): F[Either[String, Option[MerkleTrieNode]]]
+  }
+
+  object NodeStore {
+    def apply[F[_]](implicit ns: NodeStore[F]): NodeStore[F] = ns
+  }
+
+  final case class MerkleTrieState(root: Option[Hash], diff: MerkleTrieStateDiff)
+  final case class MerkleTrieStateDiff(addition: Map[Hash, MerkleTrieNode], removal: Set[Hash]) {
+    def add(hash: Hash, node: MerkleTrieNode): MerkleTrieStateDiff = this.copy(addition = addition.updated(hash, node))
+    def remove(hash: Hash): MerkleTrieStateDiff =
+      if (addition contains hash) this.copy(addition = addition - hash) else this.copy(removal = removal + hash)
+  }
+  object MerkleTrieState {
+    val empty: MerkleTrieState = MerkleTrieState(None, MerkleTrieStateDiff(Map.empty, Set.empty))
+  }
+}
---
+++ node/src/test/scala/org/witnessium/core/node/crypto/MerkleTrieTest.scala
@@ -0,0 +1,181 @@
+package org.witnessium.core
+package node.crypto
+
+import scala.util.Random
+
+import cats.Id
+import cats.implicits._
+import scodec.bits.BitVector
+
+import datatype.MerkleTrieNode
+import model.Address
+import MerkleTrie._
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.rng.Seed
+import model.ModelArbitrary
+import utest._
+
+object MerkleTrieTest extends TestSuite with ModelArbitrary {
+
+  implicit val emptyNodeStore: NodeStore[Id] = new NodeStore[Id] {
+    def get(hash: Hash): Either[String, Option[MerkleTrieNode]] = Right(None)
+  }
+
+  val emptyState = MerkleTrieState.empty
+
+  def sample: (BitVector, Address) = {
+    val address = Arbitrary.arbitrary[Address].pureApply(Gen.Parameters.default, Seed.random())
+    val key = hash[Address](address)
+    (key.toBytes.bits, address)
+  }
+  val (sampleKey, sampleValue) = sample
+
+  val tests = Tests {
+    test("get from empty"){
+      val program = get[Id, Address](sampleKey)
+      val result = program.run(emptyState)
+
+      assert(result.value == Right((emptyState, None)))
+    }
+
+    test("put and get"){
+      val program = for {
+        _ <- put[Id, Address](sampleKey, sampleValue)
+        resultValue <- get[Id, Address](sampleKey)
+      } yield resultValue
+      val result = (program run emptyState).map(_._2)
+
+      assert(result.value == Right(Some(sampleValue)))
+    }
+
+    test("put several samples and from"){
+
+      val numberOfSample = 10
+      val samples = List.fill(numberOfSample)(sample)
+      val expected = samples.sortBy(_._1.toHex)
+
+      val program = for {
+        _ <- samples.traverse{ case (k, v) => put[Id, Address](k, v)}
+        resultValue <- from[Id, Address](BitVector.empty)
+      } yield resultValue
+      val result = (program run emptyState).map(_._2)
+
+
+      assert(result.value == Right(expected))
+    }
+
+    test("put 2 samples in the same branch and remove one and from") {
+
+      val sample1 = Address.fromHex("5d80ffff5c8001011833515d00017246d8ff1de1").toOption.get
+      val sample1Hash = hash[Address](sample1).bits
+      val sample2 = Address.fromHex("805f7f010137695500800106001b7f0000ff0100").toOption.get
+      val sample2Hash = hash[Address](sample2).bits
+
+      val program = for {
+        _ <- put[Id, Address](sample1Hash, sample1)
+        _ <- put[Id, Address](sample2Hash, sample2)
+        _ <- removeByKey[Id, Address](hash[Address](sample1).bits)
+        resultValue <- from[Id, Address](BitVector.empty)
+      } yield resultValue
+      val result = (program run emptyState).map(_._2)
+
+      assert(result.value == Right(List((sample2Hash, sample2))))
+
+    }
+
+    test("put some fixed samples and remove one and from"){
+
+      val samples = List(
+        "7ba17f7f018ffefa01007fa7ffde7affeaff2de8",
+        "55ff7f80015e009bff7b533b80ffffd5ffee2030",
+        "1080ff64648801831d7f00d180f9907f00ff6be5",
+      ).map(Address.fromHex).map(_.toOption.get).map{ address => (hash[Address](address).bits, address) }
+
+      val expected = samples.tail.sortBy(_._1.toHex)
+
+      val program = for {
+        _ <- samples.traverse{ case (k, v) => put[Id, Address](k, v)}
+        _ <- removeByKey[Id, Address](samples.head._1)
+        resultValue <- from[Id, Address](BitVector.empty)
+      } yield resultValue
+      val result = (program run emptyState)
+
+      if (result.map(_._2).value != Right(expected)) {
+
+        println(s"==========================================")
+        println(s"===> samples")
+        samples foreach println
+        println(s"===> expected")
+        expected foreach println
+        println(s"===> state: ${result.value}")
+        println(s"==========================================")
+      }
+
+      assert(result.map(_._2).value == Right(expected))
+    }
+
+    test("put several samples and remove one and from"){
+
+      val numberOfSample = 100
+      val samples = List.fill(numberOfSample)(sample)
+      val expected = samples.tail.sortBy(_._1.toHex)
+
+      val program = for {
+        _ <- samples.traverse{ case (k, v) => put[Id, Address](k, v)}
+        _ <- removeByKey[Id, Address](samples.head._1)
+        resultValue <- from[Id, Address](BitVector.empty)
+      } yield resultValue
+      val result = (program run emptyState).map(_._2)
+
+      if (result.value != Right(expected)) {
+
+        println(s"==========================================")
+        println(s"===> samples")
+        samples foreach println
+        println(s"===> expected")
+        expected foreach println
+        println(s"===> state: ${result.value}")
+        println(s"==========================================")
+      }
+
+      assert(result.value == Right(expected))
+    }
+
+    test("put several samples with different order and still have the same state"){
+
+      val numberOfSample = 100
+      val numberOfRemoved = 10
+      val samples = List.fill(numberOfSample)(sample)
+      val toBeRemoved = samples.take(numberOfRemoved)
+      val shuffled = Random.shuffle(samples)
+
+      val program = for {
+        _ <- samples.traverse{ case (k, v) => put[Id, Address](k, v) }
+        _ <- toBeRemoved.traverse{ case (k, _) => removeByKey[Id, Address](k) }
+        resultValue <- from[Id, Address](BitVector.empty)
+      } yield resultValue
+      val result = (program run emptyState).map(_._1)
+
+      val program2 = for {
+        _ <- shuffled.traverse{ case (k, v) => put[Id, Address](k, v) }
+        _ <- toBeRemoved.traverse{ case (k, _) => removeByKey[Id, Address](k) }
+        resultValue <- from[Id, Address](BitVector.empty)
+      } yield resultValue
+      val result2 = (program2 run emptyState).map(_._1)
+
+      if (result.value != result2.value) {
+
+        println(s"==========================================")
+        println(s"===> samples")
+        samples foreach println
+        println(s"===> shuffled")
+        shuffled foreach println
+        println(s"===> removed: $toBeRemoved")
+        println(s"===> state1: ${result.value}")
+        println(s"===> state2: ${result2.value}")
+        println(s"==========================================")
+      }
+      assert(result.value == result2.value)
+    }
+  }
+}
