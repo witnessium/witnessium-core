51377be53a89e02d49c77aa824b034cbb5bea470
sungkmi <sungkmi@gmail.com>
2019-08-31 16:36:44 +0900

    Refine MerkleTrie#from to be an Enumerator

--- node/src/main/scala/org/witnessium/core/node/crypto/MerkleTrie.scala
+++ node/src/main/scala/org/witnessium/core/node/crypto/MerkleTrie.scala
@@ -7,6 +7,7 @@ import cats.data.{EitherT, StateT}
 import cats.implicits._
 import eu.timepit.refined.refineV
 import eu.timepit.refined.api.Refined
+import io.iteratee.Enumerator
 import scodec.bits.BitVector
 import shapeless.nat._16
 import shapeless.syntax.sized._
@@ -184,36 +185,44 @@ object MerkleTrie {
   @SuppressWarnings(Array("org.wartremover.warts.Recursion"))
   def from[F[_]:NodeStore:Monad, A: ByteDecoder](
     key: BitVector
-  ): StateT[EitherT[F, String, *], MerkleTrieState, List[(BitVector, A)]] = {
+  ): StateT[EitherT[F, String, *], MerkleTrieState, Enumerator[EitherT[F, String, *], (BitVector, A)]] = {
     StateT.inspectF((state: MerkleTrieState) => state.root match {
-      case None => EitherT.rightT[F, String](List.empty)
+      case None => EitherT.rightT[F, String](Enumerator.empty)
       case Some(_) => getNode(state).flatMap{
         case MerkleTrieNode.Leaf(prefix, value) =>
           if (key <= prefix.value) EitherT.fromEither[F](ByteDecoder[A].decode(value).flatMap{
-            case DecodeResult(v, remainder) if remainder.isEmpty => Right(List((prefix.value, v)))
+            case DecodeResult(v, remainder) if remainder.isEmpty => Right(Enumerator.enumOne((prefix.value, v)))
             case result => Left(s"Decoding failure: nonEmpty remainder $result")
           })
-          else EitherT.rightT[F, String](List.empty)
+          else EitherT.rightT[F, String](Enumerator.empty)
         case MerkleTrieNode.Branch(prefix, children) =>
 
-          def runFrom(key: BitVector)(hashWithIndex: (Option[Hash], Int)): EitherT[F, String, List[(BitVector, A)]] = {
+          def runFrom(key: BitVector)(
+            hashWithIndex: (Option[Hash], Int)
+          ): EitherT[F, String, Enumerator[EitherT[F, String, *], (BitVector, A)]] = {
             from(key) runA state.copy(root = hashWithIndex._1) map (_.map{ case (key, a) =>
               (prefix.value ++ BitVector.fromInt(hashWithIndex._2, 4) ++ key, a)
             })
           }
 
-          if (key <= prefix.value) children.unsized.toList.zipWithIndex traverse runFrom(BitVector.empty) map(_.flatten)
-          else if (!prefix.value.startsWith(key)) EitherT.rightT[F, String](List.empty)
+          def flatten(
+            enums: List[Enumerator[EitherT[F, String, *], (BitVector, A)]]
+          ): Enumerator[EitherT[F, String, *], (BitVector, A)] = {
+            (Enumerator.empty[EitherT[F, String, *], (BitVector, A)] /: enums)(_ append _)
+          }
+
+          if (key <= prefix.value) children.unsized.toList.zipWithIndex traverse runFrom(BitVector.empty) map flatten
+          else if (!prefix.value.startsWith(key)) EitherT.rightT[F, String](Enumerator.empty)
           else {
             val (index1, key1) = key drop prefix.value.size splitAt 4L
             val targetChildren: List[(Option[Hash], Int)] = children.unsized.toList.zipWithIndex.drop(index1.toInt(signed = false))
             targetChildren match {
-              case Nil => EitherT.rightT[F, String](List.empty)
+              case Nil => EitherT.rightT[F, String](Enumerator.empty)
               case x :: xs =>
                 for {
                   headList <- runFrom(key1)(x)
                   tailList <- xs traverse runFrom(BitVector.empty)
-              } yield headList ::: tailList.flatten
+              } yield headList append flatten(tailList)
             }
           }
       }
