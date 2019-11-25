4011c742cb3bcc624cf6995d78fe47d9fda45249
sungkmi <sungkmi@gmail.com>
2019-11-25 21:16:22 +0900

    Refine console client

--- console/src/main/scala/org/witnessium/core/node/console.scala
+++ console/src/main/scala/org/witnessium/core/node/console.scala
@@ -5,7 +5,9 @@ import codec.byte.ByteEncoder
 import codec.circe._
 import datatype._
 import model._
+import model.api._
 import node.crypto._
+import node.crypto.Hash.ops._
 import cats.implicits._
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Method, Request, Response}
@@ -15,11 +17,11 @@ import eu.timepit.refined.numeric.NonNegative
 import io.circe.{Encoder, Json}
 import io.circe.generic.auto._
 import io.circe.refined._
-import scodec.bits.ByteVector
+import io.circe.parser.decode
 
 object console {
 
-  val client: Service[Request, Response] = Http.client.newService(s"localhost:8081")
+  val client: Service[Request, Response] = Http.client.newService(s"localhost:8080")
 
   @SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
   val networkId: NetworkId = refineV[NonNegative](BigInt(101)).toOption.get
@@ -29,17 +31,33 @@ object console {
   private val txEncoder = ByteEncoder[Transaction]
   private val txJsonEncoer = Encoder[Transaction.Verifiable]
 
-  implicit class KeyPairOps(val keyPair: KeyPair) extends AnyVal {
+  implicit class KeyPairOps(val keyPair: KeyPair) {
     def address: Address = Address.fromPublicKey(keccak256)(keyPair.publicKey)
 
-    def sendTo(txHex: String, to: List[(Address, Long)]): Transaction.Verifiable = {
+    def utxo: List[(UInt256Bytes, Long)] = {
+      val request = Request(Method.Get, s"/address/$address")
+      request.setContentTypeJson()
+      val response = Await.result(client(request))
+      val Right(addressInfo) = decode[AddressInfo](response.contentString)
+      for {
+        transaction <- addressInfo.transactions
+        (address1, amount) <- transaction.value.outputs.toList if address1 === address
+      } yield (transaction.toHash, amount.value.toLong)
+    }
+
+    def sendTo(to: List[(Address, Long)]): Transaction.Verifiable = {
+      val (inputs, amounts) = utxo.unzip
+      val balance = amounts.sum
+      val outSum = to.map(_._2).sum
+      val remainder = balance - outSum
+
+      require(remainder > 0L, s"Not enough money: sending $outSum, but only have $balance")
+
       val Right(signed) = for{
-        bytes <- ByteVector.fromHexDescriptive(txHex)
-        txHash <- UInt256Refine.from(bytes)
-        outputs <- to.traverse[Either[String, *], (Address, BigNat)]{ case (address, amount) =>
-          refineV[NonNegative](BigInt(amount)).map((address, _))
+        outputs <- ((address, remainder) :: to).traverse[Either[String, *], (Address, BigNat)]{
+          case (address, amount) => refineV[NonNegative](BigInt(amount)).map((address, _))
         }
-        tx: Transaction = Transaction(networkId, Set(txHash), outputs.toSet)
+        tx: Transaction = Transaction(networkId, inputs.toSet, outputs.toSet)
         sig <- keyPair.sign(crypto.keccak256(txEncoder.encode(tx).toArray))
       } yield Signed(sig, tx)
 
